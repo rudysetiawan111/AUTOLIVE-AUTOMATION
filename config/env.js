0c87f3b99e246051d2f4daee8867904c2bc7f9d0
rudysetiawan111 <rudysetiawan111@gmail.com>
2026-03-16 16:12:23 +0700

    Delete config directory

--- config/env.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    GOOGLE_SHEETS_API_KEY: process.env.GOOGLE_SHEETS_API_KEY,
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
-    N8N_WEBHOOK_URL: process.env.N8N_WEBHOOK_URL
-};
